1b37bf450837f2e3685d7bd65c74e73804f4188d
Mohammad Mahdi Zarei <mohammad.mz9281@gmail.com>
2025-03-29 15:38:14 +0330

    Refactor General Settings 02

--- screens/settings_screen.py
+++ screens/settings_screen.py
@@ -49,61 +49,16 @@ class Updater:
 
         self.update_dialog.text = 'Done!'
 
+
 class GeneralSettingsScreen(MDScreen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def on_enter(self):
-        self.all_pass_value = self.manager.config_manager.getboolean(
-            'SIET1010',
-            'all_pass'
-        )
-        self.ids.all_pass_btn.icon = 'check' if self.all_pass_value else 'close'
-        self.all_pass()
+        pass
 
     def save(self):
-        self.is_all_pass = False if self.ids.all_pass_btn.icon == 'check' else True
-        self.manager.config_manager.set(
-            'SIET1010',
-            'all_pass',
-            self.is_all_pass
-        )
-        if not self.is_all_pass:
-            self.manager.config_manager.set(
-                'SIET1010',
-                'high_frequency',
-                self.ids.high_freq_field.text
-            )
-            self.manager.config_manager.set(
-                'SIET1010',
-                'low_frequency',
-                self.ids.low_freq_field.text
-            )
-        self.manager.config_manager.save()
-
-    def all_pass(self):
-        is_checked = self.ids.all_pass_btn.icon == 'check'
-
-        self.ids.all_pass_btn.icon = 'close' if is_checked else 'check'
-        bg_color = self.manager.app.STALE_BLUE if is_checked else self.manager.app.PURE_LIGHT
-        text_color = self.manager.app.PURE_LIGHT if is_checked else self.manager.app.VOID_BLACK
-        opacity = .6 if is_checked else 1
-        disabled = is_checked
-
-        self.ids.all_pass_btn.md_bg_color = bg_color
-        self.ids.all_pass_btn.text_color = text_color
-        self.ids.all_pass_btn.icon_color = text_color
-        self.ids.frequencies_label.opacity = opacity
-        self.ids.low_freq_field.disabled = disabled
-        self.ids.high_freq_field.disabled = disabled
-
-    def update_freq_labels(self, target, value):
-        if target == 'low':
-            self.ids.low_freq_label.text = f'Low (kHz): {value:.2f}'
-            self.ids.high_min_label.text = f'Min: {value:.2f} kHz'
-            self.ids.high_freq_slider.min = value
-        else:
-            self.ids.high_freq_label.text = f'High (kHz): {value:.2f}'
+        return
 
 
 class AdvancedSettingsScreen(MDScreen):
